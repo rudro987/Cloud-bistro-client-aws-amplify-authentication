dd2e58b4a394e1d2767110769892168e265997b0
Tanvir Rudro <rudro987@gmail.com>
2024-04-25 22:12:06 +0600

    Payment componenet created ans stripe installed

--- src/Pages/DashBoardPages/UserDashboardPages/Payment.jsx
+++ src/Pages/DashBoardPages/UserDashboardPages/Payment.jsx
@@ -1,9 +1,22 @@
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { loadStripe } from '@stripe/stripe-js';
+import {
+    PaymentElement,
+    Elements,
+  } from '@stripe/react-stripe-js';
+
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const Payment = () => {
     return (
         <div>
             <SectionTitle heading="Payment" subHeading="Please pay your due"></SectionTitle>
+            <div>
+                <Elements stripe={stripePromise}>
+                
+                </Elements>
+            </div>
         </div>
     );
 };
